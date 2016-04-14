e7c49908454dbb52cd88fd987d5fd32a61d1a4aa
Soar <85624529@qq.com>
2016-04-14 18:35:31 +0800

    change test yml

--- src/test/java/com/xiaojiuwo/services/UsersServiceTest.java
+++ src/test/java/com/xiaojiuwo/services/UsersServiceTest.java
@@ -1,22 +1,17 @@
 package com.xiaojiuwo.services;
 
 import com.xiaojiuwo.Application;
-import com.xiaojiuwo.repositories.UserRepository;
-
 import com.xiaojiuwo.models.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.OrderingComparison.greaterThan;
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -26,8 +21,6 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
-@Transactional
 public class UsersServiceTest {
 
     @Autowired
